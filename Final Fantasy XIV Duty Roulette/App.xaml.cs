f2f3b2d194f5ee86a69e7793549e71df7e1ae2fa
Thomas-Hewitt-Work <thomas.hewitt.professional@gmail.com>
2025-05-09 21:45:53 -0700

    Add project files.

---
+++ Final Fantasy XIV Duty Roulette/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Final_Fantasy_XIV_Duty_Roulette
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
